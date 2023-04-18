26425c2ce5eebccd7bb596a994492b5d7c654166
imvikaskashyap <mvikaskashyap@gmail.com>
2023-04-18 21:08:48 +0530

    slider

--- Frontend/frontend_medicines/src/components/Home/Home/Home.js
+++ Frontend/frontend_medicines/src/components/Home/Home/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import Slider1 from "../Slider1/Slider1";
+import Slider2 from "../Slider2/Slider2";
+import Slider3 from "../Slider3/Slider3";
+import Slider4 from "../Slider4/Slider4";
 
 const Home = () => {
 	return (
 		<div>
 			<Banner />
 			<Slider1 />
+			<Slider2 />
+			<Slider3 />
+			<Slider4 />
 		</div>
 	);
 };
