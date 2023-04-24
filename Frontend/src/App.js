7468837237b95ef45ffbc80a780bf2c3255d388d
imvikaskashyap <mvikaskashyap@gmail.com>
2023-04-24 14:23:29 +0530

    footer

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import Home from "./components/Home/Home/Home";
 
 const App = () => {
-	return <div>Hello world</div>;
+	// <<<<<<< HEAD
+	return (
+		<div>
+			<Home />
+		</div>
+	);
 };
 
 export default App;
