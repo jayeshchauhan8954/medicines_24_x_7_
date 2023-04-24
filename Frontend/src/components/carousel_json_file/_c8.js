f009da69aa814264d79dcea574908bbe925be082
jayeshchauhan8954 <officialjitandrachauhan@gmail.com>
2023-04-24 15:47:32 +0530

    frontend updates by jayesh

--- Frontend/src/components/carousel_json_file/_c8.js
+++ Frontend/src/components/carousel_json_file/_c8.js
@@ -1,128 +1,152 @@
 export const carousel_8 = {
-  carousel: [
-    {
-      id: 1,
-      title: "Supradyn Immuno+ With Turmeric + Tulsi Natural",
-      MRP: "MRP ₹270.00",
-      price: "₹236.60",
-      discount: "(12%)",
-      image: require("../Assets/Carousel_assets/c-7/"),
-    },
-    {
-      id: 2,
-      title: "Glutone 1000mg Tablet 15'S",
-      MRP: "MRP ₹2599.00",
-      price: "₹2209.15",
-      discount: "(15%)",
-      image: require("./"),
-    },
-    {
-      id: 3,
-      title: "Nutriburst Hair Skin & Nails Gummies | Enriche",
-      MRP: "MRP ₹1299.00",
-      price: "₹519.99",
-      discount: "(60%)",
-      image: require("../Assets/Carousel_assets/c-7/"),
-    },
-    {
-      id: 4,
-      title: "Supradyn Immuno+ With Turmeric + Tulsi Natural",
-      MRP: "MRP ₹270.00",
-      price: "₹237.60",
-      discount: "(12%)",
-      image: require("./"),
-    },
-    {
-      id: 5,
-      title: "Omron Hem 6161 Fully Automatic Wrist Blood",
-      MRP: "MRP ₹2345.00",
-      price: "₹2040.15",
-      discount: "(13%)",
-      image: require("../Assets/Carousel_assets/c-7/"),
-    },
-    {
-      id: 6,
-      title: "Reshape Natural Health Supplement Tablets ",
-      MRP: "MRP ₹403.00",
-      price: "₹350.61",
-      discount: "(13%)",
-      image: require("./"),
-    },
-    {
-      id: 7,
-      title: "Gnc Triple Strength Fish Oil - 1500 Mg - 60 ",
-      MRP: "MRP ₹1399.00",
-      price: "₹965.31",
-      discount: "(31%)",
+	carousel: [
+		{
+			id: 1,
+			title: 'Supradyn Immuno+ With Turmeric + Tulsi Natural',
+			MRP: 'MRP ₹270.00',
+			price: '₹236.60',
+			discount: '(12%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img 1 Supradyn Imuno + with Turemic + tulsi.jpg')
+		},
+		{
+			id: 2,
+			title: "Glutone 1000mg Tablet 15'S",
+			MRP: 'MRP ₹2599.00',
+			price: '₹2209.15',
+			discount: '(15%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img 2 Gliutone 1000 mg Tabltes.jpg')
+		},
+		{
+			id: 3,
+			title: 'Nutriburst Hair Skin & Nails Gummies | Enriche',
+			MRP: 'MRP ₹1299.00',
+			price: '₹519.99',
+			discount: '(60%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img 3 nutriburst-hair-skin-nails-gummies-enriched-with-biotin-60-strawberry-flavored-gummies-2-1671744825.jpg')
+		},
+		{
+			id: 4,
+			title: 'Supradyn Immuno+ With Turmeric + Tulsi Natural',
+			MRP: 'MRP ₹270.00',
+			price: '₹237.60',
+			discount: '(12%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img-4 luciara-anti-stretch-marks-cream-to-reduce-pregnancy-stretch-marks-50-g-2-1671740763.jpg')
+		},
+		{
+			id: 5,
+			title: 'Omron Hem 6161 Fully Automatic Wrist Blood',
+			MRP: 'MRP ₹2345.00',
+			price: '₹2040.15',
+			discount: '(13%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img-5 accusure-as-bp-digital-monitor-2-1671741637.jpg')
+		},
+		{
+			id: 6,
+			title: 'Reshape Natural Health Supplement Tablets ',
+			MRP: 'MRP ₹403.00',
+			price: '₹350.61',
+			discount: '(13%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img 6 omron-hem-6161-fully-automatic-wrist-blood-pressure-monitor-with-intellisense-technology-1-1671744043.jpg')
+		},
+		{
+			id: 7,
+			title: 'Gnc Triple Strength Fish Oil - 1500 Mg - 60 ',
+			MRP: 'MRP ₹1399.00',
+			price: '₹965.31',
+			discount: '(31%)',
 
-      image: require("../Assets/Carousel_assets/c-7/"),
-    },
-    {
-      id: 8,
-      title: "Kapiva Himalayan Shilajit Resin - 20g",
-      MRP: "MRP ₹1499.00",
-      price: "₹949.02",
-      discount: "(37%)",
-      image: require("./"),
-    },
-    {
-      id: 9,
-      title: "Swisse Ultiboost Odourless Strength Wild",
-      MRP: "MRP ₹2349.00",
-      price: "₹1714.77",
-      discount: "(27%)",
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img 7 reshape-natural-health-supplement-tablets-with-coffee-bean-and-green-tea-extracts-box-of-10-1-1669634896.jpg')
+		},
+		{
+			id: 8,
+			title: 'Kapiva Himalayan Shilajit Resin - 20g',
+			MRP: 'MRP ₹1499.00',
+			price: '₹949.02',
+			discount: '(37%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img  8 gnc-triple-strength-fish-oil-1500-mg-60-enteric-coated-softgels-2-1644500813.jpg')
+		},
+		{
+			id: 9,
+			title: 'Swisse Ultiboost Odourless Strength Wild',
+			MRP: 'MRP ₹2349.00',
+			price: '₹1714.77',
+			discount: '(27%)',
 
-      image: require("../Assets/Carousel_assets/c-7/"),
-    },
-    {
-      id: 10,
-      title: "Calcimax Total Tablet 15'S",
-      MRP: "MRP ₹344.00",
-      price: "₹302.72",
-      discount: "(12%)",
-      image: require("./"),
-    },
-    {
-      id: 11,
-      title: "Bpl Breathe Ezee N5 Compressor Nebulizer",
-      MRP: "MRP ₹1620.00",
-      price: "₹1377.00",
-      discount: "(15%)",
-      image: require("../Assets/Carousel_assets/c-7/"),
-    },
-    {
-      id: 12,
-      title: "Everherb (By Pharmeasy) Ashwagandha - Immunity",
-      MRP: "MRP ₹599.00",
-      price: "₹455.24",
-      discount: "(24%)",
-      image: require("./"),
-    },
-    {
-      id: 13,
-      title: "Healthkart Hk Vitals Fish Oil 1000mg With 180mg",
-      MRP: "MRP ₹469.00",
-      price: "₹267.00",
-      discount: "(43%)",
-      image: require("./"),
-    },
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img 9 kapiva-himalayan-shilajit-resin-20g-2-1671741641.jpg')
+		},
+		{
+			id: 10,
+			title: "Calcimax Total Tablet 15'S",
+			MRP: 'MRP ₹344.00',
+			price: '₹302.72',
+			discount: '(12%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img 10 pharmeasy-multivitamin-multimineral-immunity-booster-complete-nutrition-bottle-of-60-2-1675775391.jpg')
+		},
+		{
+			id: 11,
+			title: 'Bpl Breathe Ezee N5 Compressor Nebulizer',
+			MRP: 'MRP ₹1620.00',
+			price: '₹1377.00',
+			discount: '(15%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img 11 swisse-ultiboost-odourless-strength-wild-fish-oil-omega-3-heart-brain-eye-health-200-caps-2-1654169659.jpg')
+		},
+		{
+			id: 12,
+			title: 'Everherb (By Pharmeasy) Ashwagandha - Immunity',
+			MRP: 'MRP ₹599.00',
+			price: '₹455.24',
+			discount: '(24%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img 12 ourdaily-vitamin-e-strip-of-10-capsules-for-healthy-eye-hair-and-skin-1-1671742162.jpg')
+		},
+		{
+			id: 13,
+			title: 'Healthkart Hk Vitals Fish Oil 1000mg With 180mg',
+			MRP: 'MRP ₹469.00',
+			price: '₹267.00',
+			discount: '(43%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img 13 saridon-headache-relief-tablet-strip-of-10-tablets-2-1677525566.jpg')
+		},
 
-    {
-      id: 14,
-      title: "Gnc Triflex Fast Acting - 120 Caplets",
-      MRP: "MRP ₹4099.00",
-      price: "₹2582.37",
-      discount: "(37%)",
-      image: require("./"),
-    },
+		{
+			id: 14,
+			title: 'Gnc Triflex Fast Acting - 120 Caplets',
+			MRP: 'MRP ₹4099.00',
+			price: '₹2582.37',
+			discount: '(37%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img 14 bpl-breathe-ezee-n5-compressor-nebulizer-1-1671742364.jpg')
+		},
 
-    {
-      id: 15,
-      title: "Everherb Giloy Tulsi Juice - Strengthens Body",
-      MRP: "MRP ₹399.00",
-      price: "₹199.50",
-      discount: "(50%)",
-      image: require("./"),
-    },
-  ],
+		{
+			id: 15,
+			title: 'Everherb Giloy Tulsi Juice - Strengthens Body',
+			MRP: 'MRP ₹399.00',
+			price: '₹199.50',
+			discount: '(50%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img 15 everherb-by-pharmeasy-ashwagandha-immunity-booster-capsules-anxiety-stress-relief-bottle-of-60-2-1681476033.jpg')
+		},
+		{
+			id: 16,
+			title: 'Everherb Giloy Tulsi Juice - Strengthens Body',
+			MRP: 'MRP ₹399.00',
+			price: '₹199.50',
+			discount: '(50%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img 16 healthkart-hk-vitals-fish-oil-1000mg-with-180mg-epa-and-120mg-dha-60-capsules-2-1669698762.jpg')
+		},
+		{
+			id: 17,
+			title: 'Everherb Giloy Tulsi Juice - Strengthens Body',
+			MRP: 'MRP ₹399.00',
+			price: '₹199.50',
+			discount: '(50%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img17 gnc-triflex-fast-acting-120-caplets-2-1671741903.jpg')
+		},
+		{
+			id: 18,
+			title: 'Everherb Giloy Tulsi Juice - Strengthens Body',
+			MRP: 'MRP ₹399.00',
+			price: '₹199.50',
+			discount: '(50%)',
+			image: require('../Assets/Carousel_assets/c-8 Wellness Essentials of the Week/img 18 everherb-giloy-tulsi-juice-strengthens-body-defence-system-natural-immunity-booster-1-litre-2-1680172279.jpg')
+		}
+	]
 };
