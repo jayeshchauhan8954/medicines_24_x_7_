48ba72494b000aef0892bc41e8c11f981c5cfc45
jayeshchauhan8954 <officialjitandrachauhan@gmail.com>
2023-04-24 14:11:13 +0530

    non relevent

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import LoginAndSignup from './components/loginAndSignup/LoginAndSignup/LoginAndSignup';
-import Slider from './components/Carousels/Carousel_1/Carousel_1';
+// import Slider from './components/Carousels/Carousel_1/Carousel_1';
+import Carousel from './components/Carousels/Carousel_1/Carousel_1';
+// import ProductSlider from './components/Carousels/Carousel_1/Carousel_1';
 
 const App = () => {
 	return (
 		<div className="App">
 			{/* <LoginAndSignup /> */}
-			<Slider />
+			{/* <Slider /> */}
+			<Carousel />
+			{/* <ProductSlider /> */}
 		</div>
 	);
 };
