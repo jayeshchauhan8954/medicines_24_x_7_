84b1f393887cf74e8cb900c859b567827eab5866
preeti_sam <preetikamilla@gmail.com>
2023-04-25 21:33:57 +0530

    updated

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes } from 'react-router-dom'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import './components/Header/Header.css'
-import './components/Footer/Footer.css'
-
-
+import React from "react";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import "./components/Header/Header.css";
+import "./components/Footer/Footer.css";
+import Slider1 from "./components/Carousel/Carousel_7/Carousel_7";
+import Slider2 from "./components/Carousel/Carousel_8/Carousel_8";
+import Slider3 from "./components/Carousel/Carousel_9/Carousel_9";
 
 const App = () => {
   return (
     <div className="App">
-    <Router>
-      <Header/>
-      <Routes>
-      </Routes>
-      <Footer/>
+      <Router>
+        <Header />
+
+        <Routes></Routes>
+        <Slider1 />
+        <Slider2 />
+        <Slider3 />
+
+        <Footer />
       </Router>
-
     </div>
-  )
-}
+  );
+};
 
-
-export default App
+export default App;
